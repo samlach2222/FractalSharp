2ff78fba695544eb45713dcef87d451c077e8188
samlach2222 <lachaudsamuel@protonmail.com>
2022-11-15 17:11:04 +0100

    Add sample MPI communication and a Complex Number class

---
+++ Complex.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBrot
+{
+    internal class Complex
+    {
+        private double real = 0; // default value 0
+        private double imag = 0; // default value 0
+        
+        public Complex() { } // empty constructor --> 0 + 0i 
+
+        public Complex(double real, double imag)
+        {
+            this.real = real;
+            this.imag = imag;
+        }
+
+        /**
+        * Method to get conjugate
+        */
+        public void conjugate() { imag = -imag; }
+
+        /**
+         * Method to add a complex number in current complex number
+         * @param c complex to add
+         * @return sum of the 2 complexes
+         */
+        public Complex add(Complex c)
+        {
+            // Le résultat est aussi un complexe, il faut donc introduire une autre variable de type Complex
+            Complex sum = new Complex();
+            sum.real = this.real + c.real;
+            sum.imag = this.imag + c.imag;
+            return sum;
+        }
+
+        /**
+         * Method to divide current complex number with param passed complex number
+         * @param c divider of the division
+         * @return quotient of the division
+         */
+        public Complex divide(Complex c)
+        {
+            double x = this.real * c.real + this.imag * c.imag;
+            double y = this.imag * c.real - this.real * c.imag;
+            double z = c.real * c.real + c.imag * c.imag;
+            this.real = x / z;
+            this.imag = y / z;
+            return this;
+        }
+
+        /**
+         * Method to substract current complex number with param passed complex number
+         * @param c complex to substract
+         * @return substract
+         */
+        public Complex minus(Complex c)
+        {
+            // The result is also a complex number, so we have to introduce a new Complex
+            Complex sum = new Complex();
+            sum.real = this.real - c.real;
+            sum.imag = this.imag - c.imag;
+            return sum;
+        }
+
+        /**
+         * Method to multiply current complex number with param passed complex number
+         * @param c multiplier
+         * @return product of the 2 complexes
+         */
+        public Complex multiply(Complex c)
+        {
+            Complex product = new Complex();
+            product.real = (this.real * c.real) - (this.imag * c.imag);
+            product.imag = (this.real * c.imag) + (this.imag * c.real);
+            return product;
+        }
+
+        /**
+         * Method to multiply current complex number with a parm passed double number
+         * @param d multiplier
+         * @return product of the complex number and the double number
+         */
+        public Complex multiply(double d)
+        {
+            Complex product = new Complex();
+            product.real = this.real * d;
+            product.imag = this.imag * d;
+            return product;
+        }
+
+        /**
+         * Getter of the imaginary part of the current complex number
+         * @return imaginary part of the current complex number
+         */
+        public double getImag()
+        {
+            return imag;
+        }
+
+        /**
+         * Setter of the imaginary part of the current complex number
+         * @param imaginary part of the current complex number
+         */
+        public void setImag(double imag)
+        {
+            this.imag = imag;
+        }
+
+        /**
+         * Getter of the real part of the current complex number
+         * @return real part of the current complex number
+         */
+        public double getReal()
+        {
+            return real;
+        }
+
+        /**
+         * Getter of the real part of the current complex number
+         * @param real part of the current complex number
+         */
+        public void setReal(double real)
+        {
+            this.real = real;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Concurrent;
+using System.Collections;
+using MPI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (MPI.Environment environment = new MPI.Environment(ref args))
+        {
+            Intracommunicator comm = Communicator.world;
+
+            if (comm.Rank == 0)
+            {
+                int[] numbers = new int[comm.Size];
+
+                for (int k = 0; k < numbers.Length; k++)
+                    numbers[k] = k * k;
+
+                int r = comm.Scatter(numbers);
+
+                Console.WriteLine("Received {0} at {1}", r, comm.Rank);
+            }
+            else
+            {
+                int r = comm.Scatter<int>(0);
+                Console.WriteLine("Received {0} at {1}", r, comm.Rank);
+            }
+        }
+    }
+}
+
+
+/*
+ * LINKS :
+ * https://stackoverflow.com/questions/20710851/how-can-i-scatter-an-object-array-using-mpi-net
+ * https://nanohub.org/resources/5641/download/2008.09.04
+ * 
+ * TO RUN : 
+ * mpiexec -n 8 FractalSharp.exe
+ */
