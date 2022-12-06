97c17147449b20b09bbf9fd85f7e104c70210751
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-12-06 11:26:41 +0100

    Don't create Complex class two times

--- FractalPlusPlus/FractalPlusPlusMPI/Complex.h
+++ FractalPlusPlus/FractalPlusPlusMPI/Complex.h
@@ -1,10 +1,23 @@
 #pragma once
+/// <summary>
+/// Class to represent a complex number
+/// </summary>
 class Complex
 {
+private:
+	/// <summary>
+	/// Real part of the complex
+	/// </summary>
+	double real;
+
+	/// <summary>
+	/// Imaginary part of the complex
+	/// </summary>
+	double imag;
 public:
-	Complex(double real, double imag);
 	Complex();
+	Complex(double, double);
 	double Modulus();
-	double NextIteration(Complex c);
+	Complex NextIteration(Complex);
 };
 
